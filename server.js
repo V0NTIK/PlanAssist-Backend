8a38405ae059bf7ac966e311ef132cffa3b61e03
V0NTIK <ocwyman@gmail.com>
2026-02-24 18:16:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-cron-secret']
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
